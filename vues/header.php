769e8b33fd017888e61988c4dc39748cf530f1c3
Toumtoum <mehdi.toumia@yahoo.com>
2017-11-10 16:39:15 +0100

    transfer page done

---
+++ vues/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="nav-extended">
+            <div class="nav-wrapper">
+                <h3>Bank</h3>
+            </div>
+            <div class="nav-content">
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul class="tabs tabs-transparent hide-on-med-and-down">
+                    <li class="tab" class="active"><a href="">ADD ACCOUNT</a></li>
+                    <li class="tab"><a href="">TRANSFER</a></li>
+                    <li class="tab"><a href="">WITHDRAWAL</a></li>
+                    <li class="tab"><a href="">DELETE ACCOUNT</a></li>
+                </ul>
+                <ul class="side-nav" id="mobile-demo">
+                    <li class="tab" class="active"><a href="">ADD ACCOUNT</a></li>
+                    <li class="tab"><a href="">TRANSFER</a></li>
+                    <li class="tab"><a href="">WITHDRAWAL</a></li>
+                    <li class="tab"><a href="">DELETE ACCOUNT</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
---
+++ vues/home.php
@@ -0,0 +1,23 @@
+<main>
+    <div class="title">
+        <h2>Accounts<h2>
+    </div>
+
+    <table class="striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Account Number</th>
+                <th>Sold</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</main>
